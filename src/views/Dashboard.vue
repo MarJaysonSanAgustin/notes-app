06a5aaef8348819c1e7a6c417d7b3c6b83a434f3
Mar Jayson San Agustin <sanagustinjayson@gmail.com>
2020-05-14 19:56:00 +0800

    feat(dashboard): add dashboard view

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p>Dashboard</p>
+    <button type="button" @click="logout()">Logout</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import firebase from "firebase/app";
+import "firebase/auth";
+
+export default Vue.extend({
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: null
+    };
+  },
+  methods: {
+    logout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.replace("/");
+        });
+    }
+  }
+});
+</script>
